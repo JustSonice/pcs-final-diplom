b59505d739b59da6ca8bd9daf6fbd141591f90c4
Stanislav Chagin <windowsme5@gmail.com>
2023-05-20 14:24:43 +0300

    First Build

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,118 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    Map<String, List<PageEntry>> indexing = new HashMap<>();
+    Set<String> unusefulWords = new HashSet<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
+
+        File folder = pdfsDir;
+        String text;
+        String[] words;
+        String fileName;
+        String absFileName;
+
+        for (File file : folder.listFiles()) {
+            fileName = file.getName();
+            absFileName = file.getAbsolutePath();
+            var doc = new PdfDocument(new PdfReader(absFileName));
+            for (int i = 1; i <= doc.getNumberOfPages(); i++) {
+                var page = doc.getPage(i);
+                text = PdfTextExtractor.getTextFromPage(page);
+                words = text.split("\\P{IsAlphabetic}+");
+
+                Map<String, Integer> freqs = new HashMap<>();
+                for (var word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+
+                for (String word : freqs.keySet()) {
+                    PageEntry pageEntry = new PageEntry(fileName, i, freqs.get(word));
+                    List<PageEntry> pageEntryList = indexing.getOrDefault(word, new ArrayList<>());
+                    pageEntryList.add(pageEntry);
+                    Collections.sort(pageEntryList);
+                    indexing.put(word, pageEntryList);
+                }
+            }
+
+        }
+
+    }
+
+    private List<PageEntry> addLists(List<PageEntry> list1, List<PageEntry> list2) {
+        List<PageEntry> res = new ArrayList<>();
+        List<PageEntry> tempList2 = new ArrayList<>(list2);
+
+        for (PageEntry pageEntry : list1) {
+            boolean bothPage = false;
+            int count = -1;
+            for (int j = 0; j < tempList2.size(); j++) {
+                if (tempList2.get(j).getPdfName().equals(pageEntry.getPdfName()) &&
+                        tempList2.get(j).getPage() == pageEntry.getPage()) {
+                    res.add(new PageEntry(pageEntry.getPdfName(), pageEntry.getPage(), pageEntry.getCount() + tempList2.get(j).getCount()));
+                    bothPage = true;
+                    count = j;
+                    break;
+                }
+            }
+            if (!bothPage) {
+                res.add(pageEntry);
+            } else {
+                tempList2.remove(count);
+            }
+        }
+
+        res.addAll(tempList2);
+
+        Collections.sort(res);
+        return res;
     }
 
     @Override
-    public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+    public List<PageEntry> search(String text) {
+        String[] words = text.split("\\P{IsAlphabetic}+");
+        List<String> cleanWords = new ArrayList<>();
+
+        for (String word : words) {
+            if (word.isEmpty()) {
+                continue;
+            }
+            if (!unusefulWords.contains(word)) {
+                cleanWords.add(word);
+            }
+        }
+
+        List<PageEntry> result = new ArrayList<>();
+        for (String word : cleanWords) {
+            result = addLists(result, indexing.get(word.toLowerCase()));
+        }
+
+        return result;
+    }
+
+
+    public void readUnusefulWords(String fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String s;
+            while ((s = br.readLine()) != null) {
+                unusefulWords.add(s);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,46 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+        engine.readUnusefulWords("stop-ru.txt");
+
+        try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            System.out.println("Сервер успешно запущен!");
+            while (true) { // в цикле(!) принимаем подключения
+                try (
+                        Socket socket = new Socket("localhost", 8989);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream())
+                ) {
+                    Scanner wordEnter = new Scanner(System.in);
+                    System.out.println("Введите слово для поиска:");
+                    String word = wordEnter.nextLine();
+                    if (word != null) {
+                        System.out.println("Поисковый запрос: " + word);
+
+                        GsonBuilder builder = new GsonBuilder();
+                        Gson gson = builder.create();
+
+                        List<PageEntry> resp = engine.search(word);
+                        System.out.println("Результаты поиска: \n " + resp);
+                        out.println(gson.toJson(resp));
+                        break;
+                    } else {
+                        return;
+                    }
+                } catch (IOException e) {
+                    System.out.println("Невозможно запустить сервер!");
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
